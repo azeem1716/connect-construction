6a7efd201d883d2b7c8c70497e339fc0a505d3b7
Muhammad Azeem Khan <121564168+muhammadazeemkhan@users.noreply.github.com>
2024-09-09 17:58:00 -0700

    added

--- js/app.js
+++ js/app.js
@@ -47,6 +47,54 @@ const swiper = new Swiper('.swiper', {
     },
 });
 
+
+
+const blogSwiper = new Swiper('.blog-swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  autoplay: {
+    delay: 2000,
+  },
+
+  speed: 400,
+  centeredSlides: true,
+  spaceBetween: 0, // Reduced space between slides
+
+  // breakpoints: {
+  //   // when window width is >= 576px (Small devices like tablets)
+  //   320: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 0, // Reduced space between slides
+  //   },
+  //   // when window width is >= 768px (Medium devices like tablets in landscape mode)
+  //   768: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 0, // Reduced space between slides
+  //   },
+  //   // when window width is >= 992px (Large devices like desktops)
+  //   992: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 0, // Reduced space between slides
+  //   },
+  //   // when window width is >= 1200px (Extra large devices like widescreen desktops)
+  //   1200: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 0, // Reduced space between slides
+  //   } },
+
+
+});
+
+
+
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     // Select elements
     const toggleButton = document.getElementById('toggleButton');
